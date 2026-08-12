52546a9e60350dc9f7a6d158791090e70072ec2d
Claus Stadler <RavenArkadon@googlemail.com>
2026-08-12 14:01:58 +0200

    Fixed Gem codes, added mappings for scheme-labels

--- merge.sh
+++ merge.sh
@@ -3,5 +3,5 @@
 (cd instances && make all)
 (cd ontology && make all)
 mkdir -p target
-cat instances/target/regiostar-2021.ttl instances/target/geometries.ttl ontology/target/labels.ttl ontology/target/aggregates.ttl > target/regiostar-merge.ttl
+cat instances/target/regiostar-2021.ttl instances/target/geometries.ttl ontology/target/code-labels.ttl ontology/target/scheme-labels.ttl ontology/target/aggregates.ttl > target/regiostar-merge.ttl
 
